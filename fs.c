87303fefb5f287c24394e95e22f9f1ebf6c3dd3a
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-03 15:48:48 +0300

    feat(fs.c): add fs_read implementation

--- fs.c
+++ fs.c
@@ -47,6 +47,27 @@ static int fs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
     return 0;
 }
 
+// Read file
+static int fs_read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+    // Check if it isn't a file
+    json_t* value = json_object_get(root_json, path + 1);
+    if (!json_is_string(value))
+        return -ENOENT;
+
+    // Get a content and lenght
+    const char* content = json_string_value(value);
+    size_t len = strlen(content);
+
+    if (offset >= len)
+        return 0;
+
+    if (offset + size > len)
+        size = len - offset;
+
+    memcpy(buf, content + offset, size);
+    return size;
+}
+
 int main(int argc, char* argv[]) {
     return 0;
 }
