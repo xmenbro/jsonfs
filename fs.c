31a2758084a50fa6353e25c64a84a5a76b3acf60
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-03 16:32:39 +0300

    feat(fs.c): add json parsing

--- fs.c
+++ fs.c
@@ -68,6 +68,58 @@ static int fs_read(const char* path, char* buf, size_t size, off_t offset, struc
     return size;
 }
 
+// Register fuse operations
+static const struct fuse_operations fops = {
+    .getattr = fs_getattr,
+    .readdir = fs_readdir,
+    .read = fs_read,
+};
+
 int main(int argc, char* argv[]) {
-    return 0;
+    json_error_t error;
+    char** fuse_argv = NULL;
+    int fuse_argc = 0;
+    char* json_file = NULL;
+
+    // Allocate memory for fuse_argv
+    fuse_argv = malloc(argc * sizeof(char*));
+    if (!fuse_argv) {
+        perror("Memory allocation failed\n");
+        return 1;
+    } 
+    
+
+    // Copy name of program
+    fuse_argv[fuse_argc++] = argv[0];
+
+    // Parse args
+    for (int i = 1; i < argc; i++) {
+        // if we found an -j option which means that the next arg will be a json file
+        if (strcmp(argv[i], "-j") == 0 && i + 1 < argc)
+            json_file = argv[++i];
+        else
+            fuse_argv[fuse_argc++] = argv[i];
+    }
+    
+    if (!json_file) {
+        fprintf(stderr, "Usage: %s -j <json_file> [fuse_options] mountpoint\n", argv[0]);
+        free(fuse_argv);
+        return 1;
+    }
+
+    // Parse JSON document
+    root_json = json_load_file(json_file, 0, &error);
+    if (!root_json) {
+        fprintf(stderr, "Can't parse json %d: %s\n", error.line, error.text);
+        free(fuse_argv);
+        return 1;
+    }
+
+    // Start fuse
+    int status = fuse_main(fuse_argc, fuse_argv, &fops, NULL);
+
+    // Free memory
+    json_decref(root_json);
+    free(fuse_argv);
+    return status;
 }
