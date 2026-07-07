3c07cf5d47c22bddffa2cc5a037b9c773ef9afeb
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-07 19:02:09 +0300

    feat(fs.h): add path_info struct and parse_path function

--- fs.h
+++ fs.h
@@ -8,6 +8,13 @@ extern json_t* root_json;
 // Fuse file operations
 extern const struct fuse_operations fops;
 
+// Path structure
+extern struct path_info {
+    json_t* parent;
+    json_t* current;
+    char* name;
+};
+
 // Get file's attributes: mode, type, size
 int fs_getattr(const char* path, struct stat* st, struct fuse_file_info* fi);
 // Read directory
@@ -15,3 +22,7 @@ int fs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
                         off_t offset, struct fuse_file_info* fi, enum fuse_readdir_flags flags);
 // Read file
 int fs_read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* fi);
+
+
+// Parse the path - path info
+struct path_info* parse_path(const char* path);
