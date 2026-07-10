2837b355fc69b9571c74e9464957847986cb734f
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-10 12:43:48 +0300

    feat(fs.h): add mutex and write function

--- fs.h
+++ fs.h
@@ -1,9 +1,15 @@
 #define FUSE_USE_VERSION 31
 #include<fuse3/fuse.h>
 #include<jansson.h>
+#include<pthread.h>
 
 // The root json
 extern json_t* root_json;
+// Mutex
+extern pthread_mutex_t json_mutex;
+// Path to json file
+extern char* json_file_path;
+
 
 // Fuse file operations
 extern const struct fuse_operations fops;
@@ -23,6 +29,8 @@ int fs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
                         off_t offset, struct fuse_file_info* fi, enum fuse_readdir_flags flags);
 // Read file
 int fs_read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* fi);
+// Write data to file
+int fs_write(const char* path, const char* buf, size_t size, off_t offset, struct fuse_file_info* fi);
 
 /* User functions */
 // Parse the path - path info
