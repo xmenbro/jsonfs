9447e91b775e396be62a85305b1ce3c1407871b4
xmenbro <morozov.sereoja2020@gmail.com>
2026-07-13 17:10:20 +0300

    feat(funcs.h): add create entry function which creates files and directories

--- funcs.h
+++ funcs.h
@@ -9,5 +9,7 @@ struct path_info* parse_path(const char* path);
 int is_directory(json_t* value);
 // Replace node
 int replace_node(struct path_info* info, json_t* node, json_t* new_node);
+// Create entry - creates file or dir
+int create_entry(const char* path, json_t* new_node);
 
 #endif
